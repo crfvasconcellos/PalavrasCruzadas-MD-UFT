a6369cb3379e6a8470d759773114eb11a0e8cc15
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-05-28 16:56:26 -0300

    Algoritimo de identificar Inicio Horizontal e Vertical

--- main.c
+++ main.c
@@ -16,11 +16,49 @@ int main(){
 };
 
 
-
-    
-
+    int contador = 0;
 
 
+    for (int i = 0; i < LINHAS; i++)
+    {
+        for (int j = 0; j < COLUNAS; j++)
+        {
+            
+            if (matriz[i][j] == 0)
+            {
 
+                //Vertical
+                if (i == 0 || matriz[i-1][j] == -1)
+                {
+                    if (matriz[i+1][j] != -1)
+                    {
+                        contador++;
+                        matriz[i][j] = contador;
+                    }
+                    
+                }
+
+                //Horizontal
+                if (j == 0 || matriz[i][j-1] == -1)
+                {
+                    if (matriz[i][j+1] != -1)
+                    {
+                      
+                        contador++;
+                        matriz[i][j] = contador;
+                        
+                    }
+                    
+                }
+                
+
+
+
+                
+            }
+
+        }
+        
+    }
 
 }
