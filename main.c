f573387a034eb003156fc834ff315195c7042cd2
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-05-28 17:06:22 -0300

    Resolução de Bug lendo além da linha/coluna

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define LINHAS 5
 #define COLUNAS 8
-
 
 void printarMatriz(int matriz[LINHAS][COLUNAS]) {
     printf("\n--- MATRIZ DE PALAVRAS CRUZADAS ---\n");
@@ -18,8 +16,6 @@ void printarMatriz(int matriz[LINHAS][COLUNAS]) {
     printf("-----------------------------------\n");
 }
 
-
-
 int main(){
 
     int matriz[LINHAS][COLUNAS] = {
@@ -32,50 +28,43 @@ int main(){
 
     printarMatriz(matriz);
 
-
     int contador = 0;
-
 
     for (int i = 0; i < LINHAS; i++)
     {
         for (int j = 0; j < COLUNAS; j++)
         {
-            
             if (matriz[i][j] == 0)
             {
+                int ja_numerou = 0;
 
                 //Vertical
                 if (i == 0 || matriz[i-1][j] == -1)
                 {
-                    if (matriz[i+1][j] != -1)
+                    if (i + 1 < LINHAS && matriz[i+1][j] != -1)
                     {
                         contador++;
                         matriz[i][j] = contador;
-                        continue;
+                        ja_numerou = 1; 
                     }
-                    
                 }
 
                 //Horizontal
                 if (j == 0 || matriz[i][j-1] == -1)
                 {
-                    if (matriz[i][j+1] != -1)
+                    if (j + 1 < COLUNAS && matriz[i][j+1] != -1)
                     {
-                      
-                        contador++;
-                        matriz[i][j] = contador;
-                        
+                        if (!ja_numerou) {
+                            contador++;
+                            matriz[i][j] = contador;
+                        }
                     }
-                    
                 }
-                
-                
             }
-
         }
-        
     }
 
     printarMatriz(matriz);
 
+    return 0;
 }
