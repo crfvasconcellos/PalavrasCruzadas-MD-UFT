e1a6bf09d22352f5ab4be889c696db2a80fb3207
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-05-28 16:40:56 -0300

    Matriz colocada

--- main.c
+++ main.c
@@ -2,9 +2,24 @@
 #include <stdlib.h>
 
 
+#define LINHAS 5
+#define COLUNAS 8
+
 int main(){
 
+    int matriz[LINHAS][COLUNAS] = {
+    { 0, -1,  0, -1, -1,  0, -1,  0},
+    { 0,  0,  0,  0, -1,  0,  0,  0},
+    { 0,  0, -1, -1,  0,  0, -1,  0},
+    {-1,  0,  0,  0,  0, -1,  0,  0},
+    { 0,  0, -1,  0,  0,  0, -1, -1}
+};
+
+
+
     
+
+
 
 
 
